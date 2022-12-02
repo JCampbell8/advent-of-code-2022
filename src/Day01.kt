5c3a38b4bcfcbda4e5b9c095bc459d446be81f0c
jcampbell <jcampbell@ctic-inc.com>
2022-12-01 21:29:27 -0500

    Day 1 commit

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
+    fun getCalorieList(input: List<String>):List<Int> {
+        val sums:MutableList<Int> = mutableListOf()
+
+        var sum = 0
+        input.forEach {
+            if (it == "") {
+                sums.add(sum)
+                sum = 0
+            } else {
+                sum += it.toInt()
+            }
+        }
+
+        if(sum > 0) {
+            sums.add(sum)
+        }
+
+        return sums
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getCalorieList(input).maxOrNull() ?: 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getCalorieList(input).sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
